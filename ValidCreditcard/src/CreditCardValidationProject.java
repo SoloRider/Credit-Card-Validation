9379c8df15919c03e723c4038dc2cf30d009c3e7
Christopher.Ferguson <Christopher.Ferguson@MU-C1-06.mullen.pvt.k12.co.us>
2015-09-03 15:16:42 -0600

    Still Trying

--- ValidCreditcard/src/CreditCardValidationProject.java
+++ ValidCreditcard/src/CreditCardValidationProject.java
@@ -6,14 +6,34 @@ public class CreditCardValidationProject
 
 		public static void main(String[] args)
 			{
-				int myCard [] = new int [16];
+				long myCard [] = new long [16];
+				long myCard1 [] = new long [16];
 				Scanner user1nput = new Scanner(System.in);
-				System.out.println("Please insert 16 numbers and see some magic in the form of a credit.");
-				int card = user1nput.nextInt();
-				for(int i = 0; i < 16; i++)
+				System.out.println("Please insert 16 numbers and see some magic.");
+				long card = user1nput.nextLong();
+				long dbob = 0;
+				long bob = card % 10;
+				for(int i = 0; i <= 15; i++)
 					{
-						myCard[i] = card;
+						if(i % 2 == 0)
+							{
+								myCard1[i] = bob;
+								dbob = bob * 2;
+								if(myCard[i] > 10)
+									{
+										dbob = dbob % 10;
+										myCard[i] = dbob;
+										dbob = dbob / 10;
+										dbob = bob + dbob;
+									}
+							}
+						else
+							{
+								dbob = card % 10;
+								myCard[i] = bob;
+								System.out.print(myCard[i]);
+							}
+						System.out.print(myCard1[i] + myCard[i]);
 					}
-				
 			}
 	}
